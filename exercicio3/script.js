b2f137a61077b3e538198ae47b6648ccd2db31c3
izabela08 <izabelalima17@gmail.com>
2022-07-26 18:56:30 -0300

    exercicios 009 strings

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-//Crie a const para a frase aqui
+const frase = ("Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"")
+console.log(frase)
+
+const novafrase = frase.replaceAll('verde', 'rosa', 'azul', "laranja")
+console.log(novafrase)
+
+const verificacao1 = novafrase.includes('verde')
+console.log("na frase tem verde? ", verificacao1)
+
+const verificacao2 = novafrase.includes('laranja')
+console.log("na frase tem laranja? ", verificacao2)
